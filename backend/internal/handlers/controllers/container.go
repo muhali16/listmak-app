0d3f82ad6ea32bf9bab7597d4e755a459ae45fc4
Muhammad Ali Mustaqim <ali_mustaqim@mri-research-ind.com>
2026-06-21 04:52:09 +0700

    feat: wire aiLogRepo and AdminController into DI container

--- backend/internal/handlers/controllers/container.go
+++ backend/internal/handlers/controllers/container.go
@@ -18,35 +18,37 @@ type Container struct {
 }
 
 func InitContainer(db *gorm.DB) *Container {
-	// init repository
+	// init repositories
 	userRepo := repository.NewUserRepository(db)
 	listmakRepo := repository.NewListmakRepository(db)
 	orderRepo := repository.NewOrderRepository(db)
 	shareRepo := repository.NewShareLinkRepository(db)
 	viewShareRepo := repository.NewViewShareRepository(db)
+	aiLogRepo := repository.NewAILogRepository(db)
 
 	// init AI service
 	var aiService services.AIService
 	apiKey := os.Getenv("FIREWORKS_API_KEY")
 	model := os.Getenv("FIREWORKS_MODEL")
 	if apiKey != "" && model != "" {
-		aiService = services.NewFireworksAIService(apiKey, model)
+		aiService = services.NewFireworksAIService(apiKey, model, aiLogRepo)
 	} else {
 		aiService = services.NewNoopAIService()
 	}
 
-	// init service
+	// init services
 	userService := services.NewUserService(userRepo)
 	listmakService := services.NewListmakService(listmakRepo)
 	orderService := services.NewOrderService(orderRepo, listmakRepo, aiService)
 	shareService := services.NewShareService(shareRepo, viewShareRepo, listmakRepo)
 
-	// init controller
+	// init controllers
 	userController := NewUserController(userService)
 	authController := NewAuthController(userService)
 	listmakController := NewListmakController(listmakService)
 	orderController := NewOrderController(orderService)
 	shareController := NewShareController(shareService, orderService)
+	adminController := NewAdminController(aiLogRepo, userRepo)
 
 	return &Container{
 		UserController:    userController,
@@ -54,5 +56,6 @@ func InitContainer(db *gorm.DB) *Container {
 		ListmakController: listmakController,
 		OrderController:   orderController,
 		ShareController:   shareController,
+		AdminController:   adminController,
 	}
 }
